e6164d76a8b976c19b9e8b866d0b732c00502039
Liam Argent <liargent@hotmail.com>
2019-11-07 21:32:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes = {};
 
 function updateObjectWithKeyAndValue(object, key, value) {
-  var newObj = {};
-  newObj[key] = value;
+  var newObj = {}
+  newObj[key] = value;;
   return Object.assign({}, object, newObj);
 }
 
